10d692c22ce6b9261743e3bd9c78dd96b558d1ae
simpson <450054@naver.com>
2020-12-11 04:22:10 +0900

    03.paintg_v1.0.0

---
+++ app.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width=500;
+canvas.height = 600;
+
+ctx.strokStyle = "#black";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x =event.offsetX;
+    const y =event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
+    else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting)
+}
+
