fe6bd80f0e2481537a3186102854fdf2d56c3e51
simpson <450054@naver.com>
2020-12-15 03:40:42 +0900

    04.painting_v1.1.0_add_changingColor

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
 //context 캔버스를 컨트롤함
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 //pixel nodifier 브라우져에게 캔버스의 사이즈를 알려준다.
 canvas.width=500;
 canvas.height = 600;
 
-ctx.strokStyle = "#black";
+ctx.strokeStyle = "#black";
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -23,13 +24,13 @@ function onMouseMove(event){
     const x =event.offsetX;
     const y =event.offsetY;
     if(!painting){
-        console.log("creating path in",x ,y);
+        //console.log("creating path in",x ,y);
         //path는 선 - path을 만들면 마우스의 x,y 좌표로 path를 옮긴다.
         ctx.beginPath();
         ctx.moveTo(x,y);
     }
     else{
-        console.log("creating line in",x ,y);
+        //console.log("creating line in",x ,y);
         //path 이전의 위치에서 지금 위치까지 선을 만든다.
         //한 픽셀 씩 만든다.
         ctx.lineTo(x,y);
@@ -42,10 +43,19 @@ function onMouseDown(event){
     painting = true;
 }
 
+function handleColorClick(event){
+   const color = event.target.style.backgroundColor;
+   console.log(color);
+   ctx.strokeStyle = color;
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
+
+//object로부터 array를 만든다.
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
 
